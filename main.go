ba86cc99c065a30ec443eec99811a52e9ce8b229
Peter Johansson <peter.johansson@abc.se>
2021-11-16 23:21:03 +0100

    ok

--- main.go
+++ main.go
@@ -35,57 +35,60 @@ func getSentinelAddrs() []string {
 }
 
 func main() {
-
 	rdb := redis.NewFailoverClient(&redis.FailoverOptions{
-		Password:     "str0ng_passw0rd",
-		DialTimeout:  time.Second * 1,
-		ReadTimeout:  time.Second * 1,
-		WriteTimeout: time.Second * 1,
-		DB:           0,
-
+		Password:      "str0ng_passw0rd",
 		MasterName:    "mymaster",
+		DB:            0,
 		SentinelAddrs: getSentinelAddrs(),
 	})
 	ctx := context.Background()
 
-	go func() {
-		x := 3
-		for {
-			x++
-			s := &Something{
-				Added:  time.Now(),
-				Number: x,
-			}
-
-			if err := rdb.Set(ctx, "mykey", s, time.Duration(0)).Err(); err != nil {
-				fmt.Printf("fail to set key: %s\n", err)
-			}
-
-			time.Sleep(time.Millisecond * 200)
-		}
-	}()
-
-	go func() {
-		for {
-			time.Sleep(time.Millisecond * 200)
-			res := rdb.Get(ctx, "mykey")
-			if err := res.Err(); err != nil {
-				fmt.Printf("fail to get value for key, %s\n", err)
-				continue
-			}
-
-			s := Something{}
-			if err := res.Scan(&s); err != nil {
-				fmt.Printf("fail to unmarshal %s\n", err)
-				continue
-			}
-			fmt.Printf("%+v\n", s)
-		}
-	}()
+	go setkey(rdb, ctx)
+	go getkey(rdb, ctx)
 
 	forever := make(chan bool)
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
 	<-forever
 
+}
+
+func getkey(rdb *redis.Client, ctx context.Context) {
+	for {
+		time.Sleep(time.Millisecond * 200)
+
+		res := rdb.Get(ctx, "mykey")
+		if err := res.Err(); err != nil {
+			fmt.Printf("fail to get value for key, %s\n", err)
+			continue
+		}
+
+		s := Something{}
+		if err := res.Scan(&s); err != nil {
+			fmt.Printf("fail to unmarshal %s\n", err)
+			continue
+		}
+		fmt.Printf("%+v\n", s)
+	}
+}
+
+func setkey(rdb *redis.Client, ctx context.Context) {
+	x := 3
+
+	for {
+		time.Sleep(time.Millisecond * 200)
+
+		s := &Something{
+			Added:  time.Now(),
+			Number: x,
+		}
+		expire := time.Duration(0)
+		key := "mykey"
+
+		if err := rdb.Set(ctx, key, s, expire).Err(); err != nil {
+			fmt.Printf("fail to set key:%s %s\n", key, err)
+		}
+
+		x++
+	}
 }
